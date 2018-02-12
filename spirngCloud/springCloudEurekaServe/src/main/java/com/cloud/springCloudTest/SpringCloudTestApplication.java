74b28bc366f4c77e0f77282d6e0ddbeaa0fa4223
ParkerHuang <ParkerHuang@PC00016.infinitus-tech.com>
2018-02-12 14:41:09 +0800

    sample for spring cloud client and serve

---
+++ spirngCloud/springCloudEurekaServe/src/main/java/com/cloud/springCloudTest/SpringCloudTestApplication.java
@@ -0,0 +1,14 @@
+package com.cloud.springCloudTest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@EnableEurekaServer
+@SpringBootApplication
+public class SpringCloudTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCloudTestApplication.class, args);
+	}
+}
